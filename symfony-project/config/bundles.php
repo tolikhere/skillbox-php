61c52c65906c8687da83eda98035c2e0304b2dd5
Анатолий Павленко <fenixdurangoadrenalin@gmail.com>
2023-04-29 07:41:30 +0300

    Implement security LogingForm, Created User entity, Created two routes /login and /logout

--- symfony-project/config/bundles.php
+++ symfony-project/config/bundles.php
@@ -15,4 +15,6 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
     BabDev\PagerfantaBundle\BabDevPagerfantaBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
 ];
