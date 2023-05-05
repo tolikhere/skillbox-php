31133477dad6860b612961352084d53451a53e03
Анатолий Павленко <fenixdurangoadrenalin@gmail.com>
2023-05-05 08:01:09 +0300

    Create getAvatarUrl for User entity, Created a user admin@symfony.skillbox, Created /admin page

---
+++ symfony-project/src/Controller/AccountControllerPhpController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+#[IsGranted('IS_AUTHENTICATED_REMEMBERED')]
+class AccountControllerPhpController extends AbstractController
+{
+    #[Route('/account', name: 'app_account')]
+    public function index(): Response
+    {
+        return $this->render('account/index.html.twig', []);
+    }
+}
--- symfony-project/src/Controller/Api/ArticleContentProviderController.php
+++ symfony-project/src/Controller/Api/ArticleContentProviderController.php
@@ -3,17 +3,32 @@
 namespace App\Controller\Api;
 
 use App\Homework\ArticleContentProviderInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ArticleContentProviderController extends AbstractController
 {
-    #[Route('/api/v1/article-content/', name: 'api_article_content', methods: ['POST'])]
-    public function index(Request $request, ArticleContentProviderInterface $articleContentProvider): JsonResponse
-    {
+    /**
+     *@method User|null getUser()
+     */
+    #[Route('/api/v1/article_content', name: 'api_article_content_provider', methods: ['POST'])]
+    public function index(
+        Request $request,
+        ArticleContentProviderInterface $articleContentProvider,
+        LoggerInterface $apiLogger
+    ): JsonResponse {
+        $user = $this->getUser();
+        if (! $this->isGranted('ROLE_API')) {
+            $apiLogger->warning('User {userName} has tried to access /api/v1/article_content', [
+                'userName' => $user?->getFirstName() ?? null,
+                'roles' => $user?->getRoles() ?? null,
+                'isAuthorized' => (bool) $user,
+            ]);
+            throw $this->createAccessDeniedException('You Shall Not Pass!');
+        }
         $data = $request->toArray();
         $paragraphs = $data['paragraphs'] ?? 0;
         $word = $data['word'] ?? null;
